3322938ed642eaae848caae2c4822516373f6a6f
Greg <gbugaj@rmsweb.com>
2020-11-10 11:07:59 -0600

    Initial commit

---
+++ data/dataset-splitter.py
@@ -0,0 +1,42 @@
+import shutil
+import os
+import argparse
+import cv2
+import numpy as np
+
+
+def imwrite(path, img):
+    try:
+        cv2.imwrite(path, img)
+    except Exception as ident:
+        print(ident)
+
+
+def split(src_mulitpage_tif, target_dir):
+    print("Spliting : %s" % (src_mulitpage_tif))
+
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+
+    loaded, frames = cv2.imreadmulti(src_mulitpage_tif, [], cv2.IMREAD_ANYCOLOR)
+    if not loaded:
+        raise Exception('Unable to load source file : %s' % (src_mulitpage_tif))
+
+    print("Total frames : %s" % (len(frames)))
+
+    for idx, frame in enumerate(frames):
+        target = os.path.join(target_dir, "%s.png" %(idx))
+        print(target)
+        imwrite(target, frame)
+
+    # filename = str(uuid.uuid4())
+    # imwrite(os.path.join(hash_dir, "%s.tif" % (filename)), snip)
+    # cv2.imwrite(hash_dir, segment)
+    # showAndDestroy('frame ', snip)
+
+
+if __name__ == '__main__':
+    split('./raw/train-volume.tif', './nerve/train/image')
+    split('./raw/train-labels.tif', './nerve/train/mask')
+    # split('./raw/train-labels.tif', './ma')
+    # extractSnippetsFromCache()
---
+++ data/raw/README.md
@@ -0,0 +1,7 @@
+
+train-volume.tif (7.5 MB)	Orginal training image, 8-bit grayscale, 512x512x30 pixels
+train-labels.tif (7.5 MB)	Training image labels (0 - membranes, 255 - non-membranes), 8-bit grayscale, 512x512x30 pixels
+test-volume.tif (7.5 MB)	Test image, 8-bit grayscale, 512x512x30 pixels
+
+
+The training and test datasets are two stacks of 30 sections from a serial section Transmission Electron Microscopy (ssTEM) data set of the Drosophila first instar larva ventral nerve cord (VNC). The microcube measures 2 x 2 x 1.5 microns approx., with a resolution of 4x4x50 nm/pixel.
